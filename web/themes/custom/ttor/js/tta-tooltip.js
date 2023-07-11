16b55887e946e45f005a60586ddcf785ac90e84f
Ethan Teague <metrowebz@gmail.com>
2023-07-11 19:24:07 -0400

    Second round of revision.

--- web/themes/custom/ttor/js/tta-tooltip.js
+++ web/themes/custom/ttor/js/tta-tooltip.js
@@ -11,11 +11,11 @@
 
       const tab = document.querySelector(".tooltip-trigger");
       const tooltip = document.querySelector(".tooltip-div");
-      tab.addEventListener("mouseenter", (e) => {
-        tooltip.classList.add('show-tooltip')
+      tab?.addEventListener("mouseenter", (e) => {
+        tooltip?.classList.add('show-tooltip')
       })
-      tab.addEventListener("mouseleave", (e) => {
-        tooltip.classList.remove('show-tooltip')
+      tab?.addEventListener("mouseleave", (e) => {
+        tooltip?.classList.remove('show-tooltip')
       })
     }
   };

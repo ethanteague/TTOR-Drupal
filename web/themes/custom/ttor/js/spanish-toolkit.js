58328090b3c6c8cbfbe3df669083934601dd457b
Ethan Teague <metrowebz@gmail.com>
2024-11-24 15:41:34 -0500

    Update toolkits

---
+++ web/themes/custom/ttor/js/spanish-toolkit.js
@@ -0,0 +1,78 @@
+/**
+ * @file
+ * ttor_custom behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.ttorResourcesSpanish = {
+    attach: function (context, settings) {
+      const tabs = document.querySelectorAll(".views-field .views-field-name div");
+      const tid19s = document.querySelectorAll(".resource-box.tid-19");
+      const tid20s = document.querySelectorAll(".resource-box.tid-20");
+      const tid21s = document.querySelectorAll(".resource-box.tid-21");
+      const blockTitle = document.getElementById('download-h2-text');
+      tabs.forEach(tab => {
+        tab.addEventListener("click", () => {
+          if (tab.classList.contains("tid-19")) {
+            tid21s.forEach(tid21 => {
+              tid21.style.display = "none";
+            })
+            blockTitle.innerHTML = "Descargar Powerpoints";
+            tid20s.forEach(tid20 => {
+              tid20.style.display = "none";
+            })
+            tid19s.forEach(tid19 => {
+              tid19.style.display = "block";
+            })
+          }
+          if (tab.classList.contains("tid-20")) {
+            tid19s.forEach(tid19 => {
+              tid19.style.display = "none";
+            })
+            blockTitle.innerHTML = "Descargar Guías del kit de herramientas";
+            tid21s.forEach(tid21 => {
+              tid21.style.display = "none";
+            })
+            tid20s.forEach(tid20 => {
+              tid20.style.display = "block";
+            })
+          }
+          if (tab.classList.contains("tid-21")) {
+            tid19s.forEach(tid19 => {
+              tid19.style.display = "none";
+            })
+            blockTitle.innerHTML = "Descargar Hojas Informativas";
+            tid20s.forEach(tid20 => {
+              tid20.style.display = "none";
+            })
+            tid21s.forEach(tid21 => {
+              tid21.style.display = "block";
+            })
+          }
+        })
+      });
+
+      window.addEventListener('load', () => {
+        tid19s.forEach(tid19 => {
+          tid19.classList.add('show-tab')
+        })
+        tid20s.forEach(tid20 => {
+          tid20.style.display = "none";
+        })
+        tid21s.forEach(tid21 => {
+          tid21.style.display = "none";
+        })
+      })
+
+      window.addEventListener('click', () => {
+        tid19s.forEach(tid19 => {
+          tid19.classList.remove('show-tab')
+        })
+      })
+
+    }
+  };
+
+}(Drupal));
---
+++ web/themes/custom/ttor/js/toolkit.js
@@ -0,0 +1,78 @@
+/**
+ * @file
+ * ttor_custom behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.ttorResources = {
+    attach: function (context, settings) {
+      const tabs = document.querySelectorAll(".views-field .views-field-name div");
+      const tid16s = document.querySelectorAll(".resource-box.tid-16");
+      const tid17s = document.querySelectorAll(".resource-box.tid-17");
+      const tid18s = document.querySelectorAll(".resource-box.tid-18");
+      const blockTitle = document.getElementById('download-h2-text');
+      tabs.forEach(tab => {
+        tab.addEventListener("click", () => {
+          if (tab.classList.contains("tid-16")) {
+            tid18s.forEach(tid18 => {
+              tid18.style.display = "none";
+            })
+            blockTitle.innerHTML = "Download Toolkit Guides";
+            tid17s.forEach(tid17 => {
+              tid17.style.display = "none";
+            })
+            tid16s.forEach(tid16 => {
+              tid16.style.display = "block";
+            })
+          }
+          if (tab.classList.contains("tid-17")) {
+            tid16s.forEach(tid16 => {
+              tid16.style.display = "none";
+            })
+            blockTitle.innerHTML = "Download Powerpoints";
+            tid18s.forEach(tid18 => {
+              tid18.style.display = "none";
+            })
+            tid17s.forEach(tid17 => {
+              tid17.style.display = "block";
+            })
+          }
+          if (tab.classList.contains("tid-18")) {
+            tid16s.forEach(tid16 => {
+              tid16.style.display = "none";
+            })
+            blockTitle.innerHTML = "Download Factsheets";
+            tid17s.forEach(tid17 => {
+              tid17.style.display = "none";
+            })
+            tid18s.forEach(tid18 => {
+              tid18.style.display = "block";
+            })
+          }
+        })
+      });
+
+      window.addEventListener('load', () => {
+        tid16s.forEach(tid16 => {
+          tid16.classList.add('show-tab')
+        })
+        tid17s.forEach(tid17 => {
+          tid17.style.display = "none";
+        })
+        tid18s.forEach(tid18 => {
+          tid18.style.display = "none";
+        })
+      })
+
+      window.addEventListener('click', () => {
+        tid16s.forEach(tid16 => {
+          tid16.classList.remove('show-tab')
+        })
+      })
+
+    }
+  };
+
+}(Drupal));

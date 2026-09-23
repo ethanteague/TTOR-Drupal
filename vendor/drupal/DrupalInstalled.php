38b446510465d5dc1d2e826222470b3b7266711d
Jordan Barnes <jb8893@gmail.com>
2026-09-23 18:37:26 -0400

    TTOR-28: Security updates / core 11.4.7 / drush 13.8 / twig 3.29.

--- vendor/drupal/DrupalInstalled.php
+++ vendor/drupal/DrupalInstalled.php
@@ -16,6 +16,6 @@ class DrupalInstalled {
   /**
    * A hash of all the installed packages and their versions.
    */
-  public const string VERSIONS_HASH = 'bab915ef91d61bc1';
+  public const string VERSIONS_HASH = 'aae6dcf53ff9b4b9';
 
 }
